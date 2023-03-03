f3a7f02ae6955831399ed5f6686dfb9cd3436b27
Mariusz Trocewicz <throcu@gmail.com>
2023-03-03 01:35:10 +0100

    Add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
