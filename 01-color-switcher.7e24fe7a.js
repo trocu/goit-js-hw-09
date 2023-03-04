9728c6b26988216b476d626772016872ec92e5c1
trocu <throcu@gmail.com>
2023-03-04 00:58:14 +0000

    Deploying to gh-pages from @ trocu/goit-js-hw-09@d61a2722f3c832171de9cbbc901b1b647018bd31 🚀

---
+++ 01-color-switcher.7e24fe7a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");let d=null;e.addEventListener("click",(()=>{d=setInterval((()=>{a.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.disabled=!0})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.7e24fe7a.js.map
