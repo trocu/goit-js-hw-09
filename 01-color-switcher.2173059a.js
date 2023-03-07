67fadbe4eaae9493dba174ac4ca4dfccde18042a
trocu <throcu@gmail.com>
2023-03-07 00:29:39 +0000

    Deploying to gh-pages from @ trocu/goit-js-hw-09@9a76e620608286f6090792406d67d82f1ab3a676 🚀

--- 01-color-switcher.2173059a.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){a=setInterval((function(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.2173059a.js.map
---
+++ 01-color-switcher.cee73db5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");var a=null;t.addEventListener("click",(function(){a=setInterval((function(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.cee73db5.js.map
