1331ae04f644b293d07703f3e849d68f32751148
Mariusz Trocewicz <throcu@gmail.com>
2023-03-09 02:03:24 +0100

    Task 3 WIP

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
+const form = document.querySelector('.form');
+const { delay, step, amount } = form.elements;
+
+// form.addEventListener('input', saveInput);
+// function saveInput(e) {
+//   const dataInput = {
+//     delay: delay.value,
+//     step: step.value,
+//     amount: amount.value,
+//   };
+//   console.log(dataInput);
+// }
+
+// form.addEventListener('submit', createPromise(amount, delay));
+
+form.addEventListener('input', e => {
+  const dataInput = {
+    delay: delay.value,
+    step: step.value,
+    amount: amount.value,
+  };
+  console.log(dataInput);
+});
+
+let counter = 0;
+let timerId = null;
+
+form.addEventListener('submit', createPromise());
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  timerId = setInterval(() => {
+    const shouldResolve = Math.random() > 0.3;
+    const promise = new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve('Hello world!');
+      } else {
+        // Reject
+        reject('Ooops!');
+      }
+    });
+    console.log(promise);
+    promise
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    //Kills interval
+    if (++counter === 5) {
+      clearInterval(timerId);
+    }
+  }, 2000);
 }
